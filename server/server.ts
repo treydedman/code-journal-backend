1d26adb2b1eaf821defaf8b7a3f0333488a83d83
Trey Dedman <treydedman@gmail.com>
2025-03-11 17:19:24 +0000

    first app.get

--- server/server.ts
+++ server/server.ts
@@ -13,6 +13,22 @@ const db = new pg.Pool({
 
 const app = express();
 
+// get all entries
+app.get('/api/entries', async (req, res, next) => {
+  try {
+    const sql = `
+    select *
+    from "entries"
+    `;
+
+    const result = await db.query(sql);
+    console.log(result);
+    res.json(result.rows);
+  } catch (err) {
+    next(err);
+  }
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
 });
