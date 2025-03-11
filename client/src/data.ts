8b894710eb1de041c20231c1470817c1f7949927
Han-Zhang-Code <zyh514312@gmail.com>
2025-03-11 22:44:55 +0000

    correction

--- client/src/data.ts
+++ client/src/data.ts
@@ -5,31 +5,31 @@ export type Entry = {
   photoUrl: string;
 };
 
-type Data = {
-  entries: Entry[];
-  nextEntryId: number;
-};
+// type Data = {
+//   entries: Entry[];
+//   nextEntryId: number;
+// };
 
-const dataKey = 'code-journal-data';
+// const dataKey = 'code-journal-data';
 
-function readData(): Data {
-  let data: Data;
-  const localData = localStorage.getItem(dataKey);
-  if (localData) {
-    data = JSON.parse(localData) as Data;
-  } else {
-    data = {
-      entries: [],
-      nextEntryId: 1,
-    };
-  }
-  return data;
-}
+// function readData(): Data {
+//   let data: Data;
+//   const localData = localStorage.getItem(dataKey);
+//   if (localData) {
+//     data = JSON.parse(localData) as Data;
+//   } else {
+//     data = {
+//       entries: [],
+//       nextEntryId: 1,
+//     };
+//   }
+//   return data;
+// }
 
-function writeData(data: Data): void {
-  const dataJSON = JSON.stringify(data);
-  localStorage.setItem(dataKey, dataJSON);
-}
+// function writeData(data: Data): void {
+//   const dataJSON = JSON.stringify(data);
+//   localStorage.setItem(dataKey, dataJSON);
+// }
 
 export async function readEntries(): Promise<Entry[]> {
   const response = await fetch('/api/entries');
@@ -55,7 +55,7 @@ export async function addEntry(entry: Entry): Promise<Entry> {
 
 export async function updateEntry(entry: Entry): Promise<Entry> {
   if (!entry.entryId) throw new Error('entryId is missing');
-  const response = await fetch(`/api/entries/:${entry.entryId}`, {
+  const response = await fetch(`/api/entries/${entry.entryId}`, {
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(entry),
@@ -66,10 +66,7 @@ export async function updateEntry(entry: Entry): Promise<Entry> {
 }
 
 export async function removeEntry(entryId: number): Promise<void> {
-  const data = readData();
-  const updatedArray = data.entries.filter(
-    (entry) => entry.entryId !== entryId
-  );
-  data.entries = updatedArray;
-  writeData(data);
+  const response = await fetch(`/api/entries/${entryId}`, { method: 'DELETE' });
+  if (!response.ok) throw new Error(`failed to fetch with entryId ${entryId}`);
+  return response.json();
 }
